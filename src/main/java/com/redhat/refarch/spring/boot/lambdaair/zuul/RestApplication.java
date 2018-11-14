e1be7bb98be13d6ce548b4c0b52cc15b8b5b8bb3
211650 <211650@01hw1006775.India.TCS.com>
2018-11-14 15:08:50 +0530

    Commit the first changes

---
+++ src/main/java/com/redhat/refarch/spring/boot/lambdaair/zuul/RestApplication.java
@@ -0,0 +1,24 @@
+package com.redhat.refarch.spring.boot.lambdaair.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class RestApplication
+{
+	private static Logger logger = Logger.getLogger( RestApplication.class.getName() );
+	public static void main(String[] args)
+	{
+		SpringApplication.run( RestApplication.class, args );
+	}
+}
